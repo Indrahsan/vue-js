d2e99928149375fffcbc9005e7f4e0ea1e5168d2
indrahsan <hasanindra71@gmail.com>
2023-07-23 21:36:05 +0700

    get data from api

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,8 @@
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link> |
-    <router-link to="/profile">Profile</router-link>
+    <router-link to="/profile">Profile</router-link> |
+    <router-link to="/post">Post List</router-link>
   </nav>
   <router-view/>
 </template>
---
+++ src/views/ApiView.vue
@@ -0,0 +1,38 @@
+
+<template>
+  <div>
+    <h1>Data dari API GET:</h1>
+    <ul v-if="responseData">
+      <li v-for="post in responseData" :key="post.id">
+        <h3>{{ post.title }}</h3>
+        <p>{{ post.body }}</p>
+      </li>
+    </ul>
+    <p v-else>Tidak ada data yang ditemukan.</p>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+const apiUrl = 'https://jsonplaceholder.typicode.com/posts';
+
+export default {
+  data() {
+    return {
+      responseData: null,
+    };
+  },
+  mounted() {
+    // Lakukan permintaan API GET saat komponen dimuat
+    axios
+      .get(apiUrl)
+      .then(response => {
+        this.responseData = response.data;
+      })
+      .catch(error => {
+        console.error('Terjadi kesalahan:', error);
+      });
+  },
+};
+
+</script>
